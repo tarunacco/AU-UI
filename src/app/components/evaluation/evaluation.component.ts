5c36fba2d641f4b22f23293012c77e12c734c11d
KT1999 <tejaakarapu@gmail.com>
2021-03-25 10:25:05 +0530

    asign and final evaluation tab

---
+++ src/app/components/evaluation/evaluation.component.ts
@@ -0,0 +1,140 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluation',
+  templateUrl: './evaluation.component.html',
+  styleUrls: ['./evaluation.component.css']
+})
+export class EvaluationComponent implements OnInit {
+  score: any[] = [];
+  sessionHeaders = [];
+  sessionHeaderName = [];
+  headers = [];
+  marksData = [];
+  finalAverageReport = {};
+  total = 0;
+  constructor(private http: HttpClient) { }
+
+  @Input()
+  batchId: number;
+
+  ngOnInit() {
+    console.log('Loaded evalution Component');
+    this.fetchMarks();
+  }
+
+  fetchMarks() {
+    let url = '/api/training/all/' + this.batchId;
+    this.http
+      .get<any[]>(url, { params: { type: 'M' } })
+      .subscribe(
+        (marks) => (
+          (this.score = marks),
+          (this.sessionHeaders = marks['sessions']),
+          this.sessionHeaders.map((seshead) => {
+            this.sessionHeaderName.push(seshead.sessionName);
+          }),
+          (this.headers = [
+            'First Name',
+            'Last Name',
+            'Email Address',
+            // ...this.sessionHeaderName,
+            'Project Marks',
+            'Assignment Average',
+             'Final Score'
+           
+          ]),
+          (this.marksData = marks['marksData']),
+          this.updateReport(),
+          console.log(marks['marksData'])
+        )
+      );
+  }
+
+  getTotalMarksAverage(column) {
+    column = column.student.studentId;
+    if (column in this.finalAverageReport) {
+      return (parseFloat(this.finalAverageReport[column]) / this.total)
+        .toFixed(2)
+        .toString();
+    }
+    return '0';
+  }
+  getTotalMarks(){
+
+  }
+
+  studentmap = {};
+  arraylistassignment = {};
+
+  updateReport() {
+    let copyOfFinalAverageReport = this.finalAverageReport;
+    for (let i = 0; i < this.marksData.length; i++) {
+      let currData = this.marksData[i];
+      let currentStudentId = currData['student']['studentId'];
+      copyOfFinalAverageReport[currentStudentId] = 0;
+      for (let key in currData) {
+        if (key == 'student') {
+          continue;
+        } else {
+          copyOfFinalAverageReport[currentStudentId] += parseInt(
+            currData[key]['marks']
+          );
+        }
+      }
+    }
+    this.finalAverageReport = copyOfFinalAverageReport;
+    this.total = this.sessionHeaderName.length;
+  }
+  marks()
+  {
+
+  }
+  getMarks(row, column) {
+    const sessionId = `${this.sessionHeaders.find((session) => session.sessionName === column)
+      .sessionId
+      }`;
+    if (row[sessionId]) {
+      return row[sessionId].marks;
+    }
+    return;
+  }
+
+  mark(row, studentId, sessionName_, previousMarks, marks) {
+    let sessId;
+    this.sessionHeaders.forEach((session) => {
+      if (session.sessionName === sessionName_) {
+        sessId = parseInt(session.sessionId);
+        return;
+      }
+    });
+
+    if (previousMarks) {
+      this.finalAverageReport[studentId] -= parseInt(previousMarks);
+      this.finalAverageReport[studentId] += parseInt(marks);
+    } else {
+      this.finalAverageReport[studentId] += parseInt(marks);
+    }
+    if (row[sessId]) {
+      row[sessId]['marks'] = marks;
+    }
+
+    const markAttendance = {
+      attendanceId: {
+        sessionId: sessId,
+        studentId: studentId,
+      },
+      marks: marks,
+    };
+
+    console.log(markAttendance);
+
+    this.http
+      .post('api/training/assignMarks', markAttendance)
+      .subscribe((res) => { });
+  }
+  getProjectMarks(row) {
+    return ;
+  }
+}
