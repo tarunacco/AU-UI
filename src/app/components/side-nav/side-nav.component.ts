3baa01cef60c272b0c450e5aba626e2c1302d5d2
Abhinav Sharma <abhinav.sharma@accolitedigital.com>
2021-05-07 16:04:21 +0530

    Updates

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -3,14 +3,14 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Router } from '@angular/router';
-
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css'],
 })
-export class SideNavComponent implements OnInit{
+export class SideNavComponent implements OnInit {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -19,38 +19,41 @@ export class SideNavComponent implements OnInit{
     );
 
   user = null;
-  pic =" http://forums.asp.net/t/896760.aspx";
+  pic = ' http://forums.asp.net/t/896760.aspx';
   isUserLoggedIn = false;
-  
-  constructor(private breakpointObserver: BreakpointObserver,  private router: Router) {
-    
-  }
+
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    public authService: AuthService
+  ) {}
+
   ngOnInit(): void {
     let tempUser = JSON.parse(
       localStorage.getItem('currentGoogleLoggedInUser')
     );
-    
+
     if (tempUser) {
       this.user = tempUser;
-       console.log(this.user.picture)
-      this.pic=this.user.picture;
+      console.log(this.user.picture);
+      this.pic = this.user.picture;
       this.isUserLoggedIn = true;
-     
-     
     }
   }
-  logoutUser(){
-       localStorage.removeItem('currentGoogleLoggedInUser')
-       this.router.navigate(['']);
+  
+  logoutUser() {
+    this.authService.signOutAndRedirect();
   }
+
   loginUser(){
-    this.router.navigate(['']); 
+    this.router.navigate(['']);
     this.reloadComponent();
   }
   reloadComponent() {
     let currentUrl = this.router.url;
-        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-        this.router.onSameUrlNavigation = 'reload';
-        this.router.navigate([currentUrl]);
-    }
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
+  }
 }
